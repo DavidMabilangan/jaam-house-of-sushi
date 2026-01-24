0479856c3cde2bc1ef1410b373842c4c161bb436
David <mabilangan.davidisaiah.css@gmail.com>
2026-01-25 01:07:42 +0800

    about us section of homepage

---
+++ app/components/pages/home/section/AboutUs.vue
@@ -0,0 +1,14 @@
+
+
+
+<template>
+    <section :class="styl['about-us']">
+        <div class="container"> about us </div>
+    </section>
+</template>
+
+<style lang="stylus" module="styl">
+    .about-us
+        padding: 100px 0
+
+</style>
