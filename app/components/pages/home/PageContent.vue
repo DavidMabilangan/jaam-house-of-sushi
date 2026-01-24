4714f9a7351dad5f7c0d18923e68955b8a797fbe
David <mabilangan.davidisaiah.css@gmail.com>
2026-01-25 01:08:56 +0800

    collect contents

--- app/components/pages/home/PageContent.vue
+++ app/components/pages/home/PageContent.vue
@@ -1,9 +1,11 @@
 <script setup>
     import { defineAsyncComponent } from 'vue';
 
-    const TopSection = defineAsyncComponent(() => import('./section/TopSection.vue'));
+    const TopSection = defineAsyncComponent(() => import('./section/TopSection'));
+    const AboutUs = defineAsyncComponent(() => import('./section/AboutUs'));
 </script>
 
 <template>
     <TopSection />
+    <AboutUs />
 </template>
