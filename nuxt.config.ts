d6c36ba77e8f99c3964305c09f833c691b7acb28
David <mabilangan.davidisaiah.css@gmail.com>
2026-01-25 01:10:25 +0800

    add css,stylus,nuxt/image and meta description, title.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,30 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
+  css: ['~/assets/default.styl'],
+
   modules: [
-    '@pinia/nuxt',
-  ]
+    '@nuxt/image',
+    '@pinia/nuxt'
+  ],
+
+  app: {
+    head: {
+      title: 'Jaam House of Sushi',
+
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          name: 'description',
+          content:
+            "Here at Jaam, you'll find a distinctly unique dining experience! We are committed to bringing you the taste of Japan. All of our food preparation procedures must meet rigorous requirements which ensure that we offer top quality products made by our highly skilled chefs."
+        }
+      ],
+
+      link: [
+        { rel: 'icon', type: 'image/webp', href: '/LOGO.webp' }
+      ]
+    }
+  }
 })
